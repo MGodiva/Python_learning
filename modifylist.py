f552c16b9fc843cfcf50d91b46ed167117e105a8
MGodiva <52006633+MGodiva@users.noreply.github.com>
2020-10-26 17:14:06 +0500

    Update modifylist.py

--- modifylist.py
+++ modifylist.py
@@ -1,16 +1,11 @@
-lst = [1,2,3,4,5,6]
+lst = [10, 5, 8, ]
 def modifylist(lst):
     l = []
-    for el in lst:
-        if int(el) % 2 == 0:
-            l.append(int()/2)
-    #return lst[:] = l
-            print(l)
-
-lst = [1,2,3,4,5,6]
-def modifylist(lst):
-    l = []
-    for i in range(len(lst)+1):
-    if int(lst[i]) % 2 == 0:
-    l.append(int(lst[i])/2)
-    print(l)
+    for i in range(len(lst)):
+        if lst[i] % 2 == 0:
+            l.append(int(lst[i]/2))
+    lst[:] = l
+modifylist(lst)
+print(lst)
+modifylist(lst)
+print(lst)
